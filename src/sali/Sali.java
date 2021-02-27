bc4d707ab3dcd1332197f519070427623820a627
laaaraty <lasse.a.raty@student.jyu.fi>
2021-02-27 13:35:52 +0200

    Sali alustavasti kunnossa

--- src/sali/Sali.java
+++ src/sali/Sali.java
@@ -13,9 +13,9 @@ package sali;
  * | - huolehtii suoritukset, liikkeet ja harjoitukset  | - Liikkeet        |
  * |   välisestä yhteistyöstä ja välittää niiden tietoja| - Harjoitukset    |
  * |   pyydettäessä                                     | - Suoritus        |
- * | - lukee ja kirjoittaa harjoittelijan tiedostoon    | - Liike           |
+ * | - lukee ja kirjoittaa harjoittelijan tiedostoon    | - Liike           | <- TODO: tiedoston lukeminen ja kirjoittaminen
  * |   pyytämällä apua avustajiltaan                    | - Harjoitus       |
- * | - osaa muuttaa liikkeen id:n liikkeen nimeksi      |                   |
+ * | - osaa muuttaa liikkeen id:n liikkeen nimeksi      |                   |<- TODO: liikkeen id:n muuttaminen liikkeen nimeksi
  * |                                                    |                   |
  * |                                                    |                   |
  * |                                                    |                   |
@@ -29,13 +29,84 @@ package sali;
  *
  */
 public class Sali {
+    private Suoritukset suoritukset = new Suoritukset();
+    
+    
+    /**
+     * Lisätään uusi suoritus
+     * @param suoritus lisättävä suoritus
+     * @throws SailoException jos lisääminen ei onnistu
+     * @example
+     * <pre name="test">
+     * #THROWS SailoException
+     * Sali sali = new Sali();
+     * Suoritus kyykkysarja1 = new Suoritus(), kyykkysarja2 = new Suoritus();
+     * kyykkysarja1.rekisteroi(); kyykkysarja2.rekisteroi();
+     * sali.getSuorituksia() === 0;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 1;
+     * sali.lisaa(kyykkysarja2); sali.getSuorituksia() === 2;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 3;
+     * sali.getSuorituksia() === 3;
+     * sali.annaSuoritus(0) === kyykkysarja1;
+     * sali.annaSuoritus(1) === kyykkysarja2;
+     * sali.annaSuoritus(2) === kyykkysarja1;
+     * sali.annaSuoritus(3) === kyykkysarja1; #THROWS IndexOutOfBoundsException
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 4;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 5;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 6;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 7;
+     * sali.lisaa(kyykkysarja1); sali.getSuorituksia() === 8;
+     * sali.lisaa(kyykkysarja1);            #THROWS SailoException
+     * </pre>
+     */
+    public void lisaa(Suoritus suoritus) throws SailoException {
+        suoritukset.lisaa(suoritus);     
+    }
+    
+    
+    /**
+     * @return suoritusten lukumäärä
+     */
+    public int getSuorituksia() {
+        return suoritukset.getLkm();
+    }
+    
 
+    /**
+     * Antaa kerhon i:nnen jäsenen
+     * @param i monesko jäsen
+     * @return jäsen paikasta i
+     */
+    public Suoritus annaSuoritus(int i) {
+        return suoritukset.anna(i);
+    }
+    
+    
     /**
      * @param args ei käytössä
      */
     public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    
+        Sali sali = new Sali();
+            
+            Suoritus kyykkysarja = new Suoritus(), kyykkysarja2 = new Suoritus();
+            kyykkysarja.rekisteroi();
+            kyykkysarja.taytaKyykkyTiedoilla();
+            kyykkysarja2.rekisteroi();
+            kyykkysarja2.taytaKyykkyTiedoilla();
+            
+            try {  
+            sali.lisaa(kyykkysarja);
+            sali.lisaa(kyykkysarja2);
+            } catch (SailoException e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("============= Salin testi =================");
+            
+            for (int i = 0; i < sali.getSuorituksia(); i++) {
+                Suoritus suoritus = sali.annaSuoritus(i);
+                suoritus.tulosta(System.out);
+            }
     }
 
 }
